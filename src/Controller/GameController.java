dc6ccf7388141997513538f4dca384a7a2126c61
osheensachdev <osheen18059@iiitd.ac.in>
2019-10-31 01:29:35 +0530

    generalized levelgui

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -9,9 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -29,11 +27,12 @@ public class GameController {
         this.game = game;
     }
 
-    public void level1(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/Level1GUI.fxml"));
+    public void level0(ActionEvent actionEvent) throws IOException {
+        int LEVEL = 0;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/LevelGUI.fxml"));
         Parent view = fxmlLoader.load();
         LevelController controller = (LevelController) fxmlLoader.getController();
-        controller.setLevel(game.getLevel(0));
+        controller.setLevel(game.getLevel(LEVEL));
         Scene viewScene = new Scene(view,600, 400);
         controller.setScene(viewScene);
         GridPane grid = (GridPane) (viewScene.lookup("#grid"));
@@ -42,7 +41,7 @@ public class GameController {
         FileInputStream input2 = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/grass2.jpg");
         Image image2 = new Image(input2);
 
-       for (String plant:  game.getLevel(0).getAvailablePlants()) {
+        for (String plant:  game.getLevel(LEVEL).getAvailablePlants()) {
             FileInputStream fileInputStream = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/" + plant + ".png");
             Image plantImage = new Image(fileInputStream);
             ImageView plantImageView = new ImageView();
@@ -57,7 +56,7 @@ public class GameController {
             });
 
         }
-        for(int i = 0; i < NUMBER_OF_ROWS; i ++) {
+        for(int i = 0; i < game.getLevel(LEVEL).NUMBER_OF_ROWS; i ++) {
             for(int j = 0; j < NUMBER_OF_COLUMNS; j ++) {
                 final int row = i;
                 final int column = j;
@@ -75,7 +74,133 @@ public class GameController {
                         }
                     }
                 });
-                GridPane.setRowIndex(imageView, row + 2);
+                GridPane.setRowIndex(imageView, row + 3 - LEVEL);
+                GridPane.setColumnIndex(imageView, column + 3);
+                if (((i+j)%2) == 0) {
+                    imageView.setImage(image);
+                }
+                else{
+                    imageView.setImage(image2);
+                }
+                grid.getChildren().add(imageView);
+            }
+        }
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(viewScene);
+        window.show();
+    }
+    public void level1(ActionEvent actionEvent) throws IOException {
+        int LEVEL = 1;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/LevelGUI.fxml"));
+        Parent view = fxmlLoader.load();
+        LevelController controller = (LevelController) fxmlLoader.getController();
+        controller.setLevel(game.getLevel(LEVEL));
+        Scene viewScene = new Scene(view,600, 400);
+        controller.setScene(viewScene);
+        GridPane grid = (GridPane) (viewScene.lookup("#grid"));
+        FileInputStream input = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/grass.jpg");
+        Image image = new Image(input);
+        FileInputStream input2 = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/grass2.jpg");
+        Image image2 = new Image(input2);
+
+        for (String plant:  game.getLevel(LEVEL).getAvailablePlants()) {
+            FileInputStream fileInputStream = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/" + plant + ".png");
+            Image plantImage = new Image(fileInputStream);
+            ImageView plantImageView = new ImageView();
+            AnchorPane plantPanel = (AnchorPane) viewScene.lookup("#plantPanel"+plant);
+            plantImageView.setFitWidth(60);
+            plantImageView.setFitHeight(60);
+            plantImageView.setImage(plantImage);
+            plantPanel.getChildren().add(plantImageView);
+            plantPanel.onMouseClickedProperty().setValue(e->{
+                controller.setPlantPicked(true);
+                controller.setPlant(plant);
+            });
+
+        }
+        for(int i = 0; i < game.getLevel(LEVEL).NUMBER_OF_ROWS; i ++) {
+            for(int j = 0; j < NUMBER_OF_COLUMNS; j ++) {
+                final int row = i;
+                final int column = j;
+                ImageView imageView = new ImageView();
+                imageView.setFitWidth(40);
+                imageView.setFitHeight(30);
+                imageView.onMouseClickedProperty().setValue(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        System.out.println(row + " " + column);
+                        try{
+                            controller.placePlant(mouseEvent,row,column);}
+                        catch (IOException e){
+                            System.out.println("boo");
+                        }
+                    }
+                });
+                GridPane.setRowIndex(imageView, row + 3 - LEVEL);
+                GridPane.setColumnIndex(imageView, column + 3);
+                if (((i+j)%2) == 0) {
+                    imageView.setImage(image);
+                }
+                else{
+                    imageView.setImage(image2);
+                }
+                grid.getChildren().add(imageView);
+            }
+        }
+
+        Stage window = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(viewScene);
+        window.show();
+    }
+    public void level2(ActionEvent actionEvent) throws IOException {
+        int LEVEL = 2;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../View/LevelGUI.fxml"));
+        Parent view = fxmlLoader.load();
+        LevelController controller = (LevelController) fxmlLoader.getController();
+        controller.setLevel(game.getLevel(LEVEL));
+        Scene viewScene = new Scene(view,600, 400);
+        controller.setScene(viewScene);
+        GridPane grid = (GridPane) (viewScene.lookup("#grid"));
+        FileInputStream input = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/grass.jpg");
+        Image image = new Image(input);
+        FileInputStream input2 = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/grass2.jpg");
+        Image image2 = new Image(input2);
+
+        for (String plant:  game.getLevel(LEVEL).getAvailablePlants()) {
+            FileInputStream fileInputStream = new FileInputStream("/Users/osheensachdev/Documents/GitHub/PlantsVsZombies/src/Controller/" + plant + ".png");
+            Image plantImage = new Image(fileInputStream);
+            ImageView plantImageView = new ImageView();
+            AnchorPane plantPanel = (AnchorPane) viewScene.lookup("#plantPanel"+plant);
+            plantImageView.setFitWidth(60);
+            plantImageView.setFitHeight(60);
+            plantImageView.setImage(plantImage);
+            plantPanel.getChildren().add(plantImageView);
+            plantPanel.onMouseClickedProperty().setValue(e->{
+                controller.setPlantPicked(true);
+                controller.setPlant(plant);
+            });
+
+        }
+        for(int i = 0; i < game.getLevel(LEVEL).NUMBER_OF_ROWS; i ++) {
+            for(int j = 0; j < NUMBER_OF_COLUMNS; j ++) {
+                final int row = i;
+                final int column = j;
+                ImageView imageView = new ImageView();
+                imageView.setFitWidth(40);
+                imageView.setFitHeight(30);
+                imageView.onMouseClickedProperty().setValue(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent mouseEvent) {
+                        System.out.println(row + " " + column);
+                        try{
+                            controller.placePlant(mouseEvent,row,column);}
+                        catch (IOException e){
+                            System.out.println("boo");
+                        }
+                    }
+                });
+                GridPane.setRowIndex(imageView, row + 3 - LEVEL);
                 GridPane.setColumnIndex(imageView, column + 3);
                 if (((i+j)%2) == 0) {
                     imageView.setImage(image);
--- src/Model/Level.java
+++ src/Model/Level.java
@@ -6,6 +6,7 @@ import java.io.*;
 public class Level {
     private int LEVEL;
 
+    public int NUMBER_OF_ROWS;
     private int NUMBER_OF_ZOMBIES;
 
     ArrayList<Zombie> zombies;
@@ -16,11 +17,13 @@ public class Level {
         switch (LEVEL) {
             case 0:
                 NUMBER_OF_ZOMBIES = 5;
+                NUMBER_OF_ROWS = 1;
                 availablePlants = new ArrayList<String>();
                 availablePlants.add("PeaShooter");
                 availablePlants.add("SunFlower");
                 break;
             case 1:
+                NUMBER_OF_ROWS = 3;
                 NUMBER_OF_ZOMBIES = 7;
                 availablePlants = new ArrayList<String>();
                 availablePlants.add("PeaShooter");
@@ -28,6 +31,7 @@ public class Level {
                 availablePlants.add("WallNut");
                 break;
             case 2:
+                NUMBER_OF_ROWS = 5;
                 NUMBER_OF_ZOMBIES = 10;
                 availablePlants = new ArrayList<String>();
                 availablePlants.add("PeaShooter");
