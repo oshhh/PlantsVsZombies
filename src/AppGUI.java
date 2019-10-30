3934b2d433726f22f41bce11925f7a12042508e5
osheensachdev <osheen18059@iiitd.ac.in>
2019-10-30 14:13:18 +0530

    hmmm

---
+++ src/AppGUI.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import java.io.IOException;
+
+
+public class AppGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AppGUI.fxml"));
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+}
