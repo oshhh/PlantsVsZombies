4bb2ff4ea4bdb83f6202aed345c123e2ad88b4d1
IshaGupta18 <ishagupta1828@gmail.com>
2019-10-31 00:04:48 +0530

    more classes

--- src/Model/Level.java
+++ src/Model/Level.java
@@ -4,7 +4,38 @@ import java.util.*;
 import java.io.*;
 
 public class Level {
-    
+    private int LEVEL;
 
-    public Level(int levelNo) {}
+    private int NUMBER_OF_ZOMBIES;
+
+    ArrayList<Zombie> zombies;
+    ArrayList<String> availablePlants;
+
+    public Level(int levelNo) {
+        LEVEL = levelNo;
+        switch (LEVEL) {
+            case 0:
+                NUMBER_OF_ZOMBIES = 5;
+                availablePlants = new ArrayList<String>();
+                availablePlants.add("PeaShooter");
+                availablePlants.add("SunFlower");
+                break;
+            case 1:
+                NUMBER_OF_ZOMBIES = 7;
+                availablePlants = new ArrayList<String>();
+                availablePlants.add("PeaShooter");
+                availablePlants.add("SunFlower");
+                availablePlants.add("WallNut");
+                break;
+            case 2:
+                NUMBER_OF_ZOMBIES = 10;
+                availablePlants = new ArrayList<String>();
+                availablePlants.add("PeaShooter");
+                availablePlants.add("SunFlower");
+                availablePlants.add("WallNut");
+                availablePlants.add("CherryBomb");
+                break;
+
+        }
+    }
 }
