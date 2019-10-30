e35d0fe618486f092d235599b96d6fee19614aae
osheensachdev <osheen18059@iiitd.ac.in>
2019-10-30 14:31:44 +0530

    added main

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Application.launch(LoginGUI.class,args);
+    }
+}
