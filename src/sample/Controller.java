28c18cddd0a3eb6f59dc7c2f8672912ff19d6025
osheensachdev <osheen18059@iiitd.ac.in>
2019-10-30 12:32:12 +0530

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,8 @@
+package sample;
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void startGame(ActionEvent actionEvent) {
+        System.out.println("Hello World");
+    }
+}
