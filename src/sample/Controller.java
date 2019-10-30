3f7546d06d224608c0cadc9b098c385644580d99
osheensachdev <osheen18059@iiitd.ac.in>
2019-10-30 12:41:00 +0530

    project completed.

--- src/sample/Controller.java
+++
@@ -1,8 +0,0 @@
-package sample;
-import javafx.event.ActionEvent;
-
-public class Controller {
-    public void startGame(ActionEvent actionEvent) {
-        System.out.println("Hello World");
-    }
-}
